f55b7872d792b12e56624cf550944ff503ebfc4f
chaim <chaimyisroel@gmail.com>
2021-08-12 09:29:22 -0400

    commit tree 3cafa6d90afa27f5231bc0d81735cdc7aa2ecabf

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(sentence =>
+    sentence.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' '))
+  }
+console.log(titleCased())
